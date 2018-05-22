01c45f5e7ed266dec81c33aa3fc396b00df99e16
Holden Altaffer <haltaffer@gmail.com>
2018-05-22 06:44:31 +0000

    automatically backed up by learn

--- db/migrate/002_create_costume_stores.rb
+++ db/migrate/002_create_costume_stores.rb
@@ -3,7 +3,7 @@ class CreateCostumeStores < ActiveRecord::Migration[5.1]
   def change
     create_table :costume_stores do |t|
       t.string :name
-      t.text :location
+      t.string :location
       t.integer :costume_inventory
       t.integer :num_of_employees
       t.boolean :still_in_business
